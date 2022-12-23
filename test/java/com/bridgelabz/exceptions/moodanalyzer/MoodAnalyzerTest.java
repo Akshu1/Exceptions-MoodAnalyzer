4802492ef8792502f19661376f1ea91e1c8ce117
akshu1 <akshathanagaraj98@gmail.com>
2022-12-24 02:26:57 +0530

    Add:TestCases to perform Custom handling(ThrowsCustomExceptionNullorEmpty)

--- test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
@@ -4,23 +4,53 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-    @Test
-    public void testMoodAnalysis_whenMoodIsSad() {
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a sad message");//Tc1.1 Refactor
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood,"SAD");
-    }
+        //  Analyse mood by passing the message in the constructor
+        @Test
+        public void testMoodAnalysis_whenMoodIsSad() {//uc1 refactor
+            MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a sad message");
+            String mood;
+            try {
+                mood = moodAnalyzer.analyseMood();
+                Assert.assertEquals("SAD" , mood);
+            } catch (MoodAnalysisException e) {
+                e.printStackTrace();
+            }
+        }
+        //Analyse mood by passing the message in the constructor
+        @Test
+        public void testMoodAnalysis_whenMoodIsHappy() {//uc1refactor
+            MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a happy message");
+            String mood;
+            try {
+                mood = moodAnalyzer.analyseMood();
+                Assert.assertEquals("HAPPY" , mood);
+            }catch (MoodAnalysisException e) {
+                e.printStackTrace();
+            }
+        }
 
-    @Test
-    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a happy message");
-        String mood1 = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood1,"HAPPY");
+        // Custom Exception if User Provides Invalid Mood
+        // Input message: null
+        @Test
+        public void testMoodAnalysis_whenMoodIsNull_ShouldThrowException() {//uc 3.1
+            MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain(null);
+            try {
+                moodAnalyzer.analyseMood();
+            } catch (MoodAnalysisException e) {
+                Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+            }
+        }
+        // Custom Exception if User Provides Invalid Mood
+        //Input message: empty
+        @Test
+        public void testMoodAnalysis_whenMoodIsEmpty_ShouldThrowException() {//uc3.2
+            MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("");
+            try {
+                moodAnalyzer.analyseMood();
+            } catch (MoodAnalysisException e) {
+                System.out.println(e);
+                System.out.println(e.type);
+                Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , e.type);
+            }
+        }
     }
-    @Test
-    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain(null);
-        String mood2 = moodAnalyzer.analyseMood();
-        Assert.assertEquals(mood2,"HAPPY");
-    }
-}
