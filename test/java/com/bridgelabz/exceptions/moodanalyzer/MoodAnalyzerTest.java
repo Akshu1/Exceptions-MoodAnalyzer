5429fa73399fb9f7cdd446e05b284dceecfdcfc4
Akshatha <49878763+Akshu1@users.noreply.github.com>
2022-12-24 01:39:51 +0530

    Update MoodAnalyzerTest.java

--- test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
@@ -6,14 +6,14 @@ import org.testng.annotations.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void testMoodAnalysis_whenMoodIsSad() throws MoodAnalysisException {
+    public void testMoodAnalysis_whenMoodIsSad() {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a sad message");//Tc1.1 Refactor
         String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals(mood,"SAD");
     }
 
     @Test
-    public void testMoodAnalysis_whenMoodIsHappy() throws MoodAnalysisException {//Tc1.2 Refactor
+    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a happy message");
         String mood1 = moodAnalyzer.analyseMood();
         Assert.assertEquals(mood1,"HAPPY");
