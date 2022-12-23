fe9b52e66543cd2c8393edc40fd2c1633210fe0f
akshu1 <akshathanagaraj98@gmail.com>
2022-12-24 02:11:39 +0530

    Add:TestCase to Handle  Null Exception

--- test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerTest.java
@@ -5,15 +5,22 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void testMoodAnalysis_whenMoodIsSad() { //TC 1.1 "I am in sad mood"
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
-        String mood = moodAnalyzer.analyseMood( "I am in sad mood ");
-        Assert.assertEquals(mood, "SAD");
+    public void testMoodAnalysis_whenMoodIsSad() {
+        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a sad message");//Tc1.1 Refactor
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals(mood,"SAD");
+    }
+
+    @Test
+    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
+        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a happy message");
+        String mood1 = moodAnalyzer.analyseMood();
+        Assert.assertEquals(mood1,"HAPPY");
     }
     @Test
-    public void testMoodAnalysis_whenMoodIsHappy() { //TC 1.2 I am in happy mood
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
-        String mood1 = moodAnalyzer.analyseMood("I am in happy mood");
-        Assert.assertEquals(mood1, "HAPPY");
+    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain(null);
+        String mood2 = moodAnalyzer.analyseMood();
+        Assert.assertEquals(mood2,"HAPPY");
     }
 }
