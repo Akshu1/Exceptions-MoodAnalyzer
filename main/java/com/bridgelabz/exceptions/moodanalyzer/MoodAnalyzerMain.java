8f39f418895b36e8b2e03c035842094dec79625d
akshu1 <akshathanagaraj98@gmail.com>
2022-12-24 00:56:21 +0530

    Add:Program to Check Mood-Happy/Sad by using Constructor in Exceptions

---
+++ main/java/com/bridgelabz/exceptions/moodanalyzer/MoodAnalyzerMain.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.exceptions.moodanalyzer;
+
+public class MoodAnalyzerMain {
+    public String message;
+
+    //Parameterized Constructor
+    public  MoodAnalyzerMain(String message) {
+        this.message = message;
+    }
+
+    //Handle Customized Exception using try-catch block
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch(NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
+        }
+    }
+}
